9561cac04fa862c8788970ac6a71dd1ea11c8800
manaf11 <manaf1127.ahmad@gmail.com>
2022-02-12 18:47:05 +0200

    sync

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo for beginner',
+      title: 'Flutter Demo for beginner, now we are not newbies',
       theme: ThemeData(
         // This is the theme of your application.
         //
